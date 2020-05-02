f4577bcbcaec13cf547899b7b3ec5df74e24a2b0
jorge alfaro gonzalez <jorgealfarogzlz@hotmail.com>
2020-05-02 17:08:24 -0400

    Nueva Rama

--- WebService_BakApp/My Project/AssemblyInfo.vb
+++ WebService_BakApp/My Project/AssemblyInfo.vb
@@ -11,8 +11,8 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyTitle("WebService_BakApp")> 
 <Assembly: AssemblyDescription("")> 
 <Assembly: AssemblyCompany("")> 
-<Assembly: AssemblyProduct("WebService_BakApp")> 
-<Assembly: AssemblyCopyright("Copyright ©  2017")> 
+<Assembly: AssemblyProduct("WebService_BakApp")>
+<Assembly: AssemblyCopyright("Copyright ©  2020")>
 <Assembly: AssemblyTrademark("")> 
 
 <Assembly: ComVisible(False)>
